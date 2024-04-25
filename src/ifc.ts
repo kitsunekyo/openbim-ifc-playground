7210f5ce7284087a33226953b759c76203fa251f
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-04-25 09:56:57 +0200

    feat: complete in-browser stream

---
+++ src/ifc.ts
@@ -0,0 +1,128 @@
+import * as OBC from "openbim-components";
+import { createConsola } from "consola/browser";
+
+type GeometryPartFileId = `${string}.ifc-processed-geometries-${number}`;
+type GlobalDataFileId = `${string}.ifc-processed-global`;
+
+type StreamedGeometries = {
+  assets: Array<{
+    id: number;
+    geometries: Array<{
+      color: number[];
+      geometryID: number;
+      transformation: number[];
+    }>;
+  }>;
+  geometries: {
+    [id: number]: {
+      boundingBox: Record<number, number>;
+      hasHoles: boolean;
+      geometryFile: GeometryPartFileId;
+    };
+  };
+  globalDataFileId: GlobalDataFileId;
+};
+
+type StreamedProperties = {
+  types: Record<number, number[]>;
+  ids: Record<
+    number,
+    {
+      boundingBox: Record<number, number>;
+      hasHoles: boolean;
+      // geometryFile: string; // "url-to-geometry-file-in-your-backend"; -> this is incorrectly documented and doesnt exist
+    }
+  >;
+  indexesFile: string; // "url-to-indexes-file-in-your-backend";
+};
+
+const logger = createConsola({
+  defaults: {
+    tag: "ifc",
+  },
+});
+
+export async function convertToStreamable(ifcFile: File) {
+  const fileUUID = crypto.randomUUID();
+  logger.log("converting", performance.now());
+  const streamer = new OBC.FragmentIfcStreamConverter(new OBC.Components());
+  streamer.settings.wasm = {
+    path: "https://unpkg.com/web-ifc@0.0.53/",
+    absolute: true,
+  };
+
+  const streamedGeometries: StreamedGeometries = {
+    assets: [],
+    geometries: {},
+    globalDataFileId: `${fileUUID}.ifc-processed-global`,
+  };
+
+  const geometryFiles: {
+    content: Uint8Array;
+    name: GeometryPartFileId | GlobalDataFileId;
+    originalName: string;
+  }[] = [];
+
+  let geometryIndex = 0;
+
+  streamer.onGeometryStreamed.add(({ buffer, data }) => {
+    logger.log("onGeometryStreamed", performance.now());
+
+    const geometryFileId: GeometryPartFileId = `${fileUUID}.ifc-processed-geometries-${geometryIndex}`;
+
+    geometryFiles.push({
+      content: buffer,
+      name: geometryFileId,
+      originalName: ifcFile.name,
+    });
+
+    for (const id in data) {
+      streamedGeometries.geometries[id] = {
+        boundingBox: data[id].boundingBox,
+        hasHoles: data[id].hasHoles,
+        geometryFile: geometryFileId,
+      };
+    }
+
+    geometryIndex++;
+  });
+
+  streamer.onAssetStreamed.add((assets) => {
+    logger.log("onAssetsStreamed", performance.now());
+
+    for (const asset of assets) {
+      streamedGeometries.assets.push({
+        id: asset.id,
+        geometries: asset.geometries,
+      });
+    }
+  });
+
+  streamer.onIfcLoaded.add(async (globalFile) => {
+    logger.log("onIfcLoaded", performance.now());
+
+    geometryFiles.push({
+      name: streamedGeometries.globalDataFileId,
+      content: globalFile,
+      originalName: ifcFile.name,
+    });
+
+    logger.success("onIfcLoaded complete", performance.now());
+    logger.info("geometryFiles", geometryFiles);
+    logger.info("streamedGeometries", streamedGeometries);
+  });
+
+  const progressEl = document.getElementById("progress") as HTMLProgressElement;
+  if (!progressEl) {
+    throw new Error('Element with id "progress" not found');
+  }
+
+  streamer.onProgress.add((value) => {
+    progressEl.value = value * 100;
+    if (value === 1) {
+      logger.success("progress 100%", performance.now());
+    }
+  });
+
+  streamer.streamFromBuffer(new Uint8Array(await ifcFile.arrayBuffer()));
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import * as fragments from "./fragments";
+import * as ifc from "./ifc";
 import "./style.css";
 
-fragments.setup();
+(function main() {
+  const formEl = document.getElementById("ifcForm") as HTMLFormElement;
+  if (!formEl) {
+    throw new Error('Element with id "ifcForm" not found');
+  }
+  const inputEl = document.getElementById("fileInput") as HTMLInputElement;
+  if (!inputEl) {
+    throw new Error('Element with id "fileInput" not found');
+  }
+
+  formEl.addEventListener("submit", (e: SubmitEvent) => {
+    e.preventDefault();
+    const file = inputEl.files?.[0];
+    if (!file) {
+      throw new Error("No file selected");
+    }
+    ifc.convertToStreamable(file);
+  });
+})();
